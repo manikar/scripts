c4789d1054efeeab772ac42525cedff022d12177
manikar <42275875+manikar@users.noreply.github.com>
2020-01-14 21:56:54 -0500

    Update nobleltebmc.sh

--- nobleltebmc.sh
+++ nobleltebmc.sh
@@ -2,7 +2,7 @@
 
 cd ~/android/aosp
 make clean
-wget -O .repo/local_manifests/roomservice.xml https://raw.githubusercontent.com/manikar/local_manifests/lineage-16.0/nobleltebmc.xml
+wget -O .repo/local_manifests/roomservice.xml https://raw.githubusercontent.com/manikar/local_manifests/aosp/nobleltebmc.xml
 repo sync -c --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
 brunch nobleltebmc
