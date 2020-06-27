dfd5ff3759d0f91e12fa0c956c4ae82fd36650dd
manikar <42275875+manikar@users.noreply.github.com>
2020-06-27 06:57:16 -0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/android/aosp
 cd ~/android/aosp
-repo init -u https://android.googlesource.com/platform/manifest -b android-9.0.0_r47
+repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r39
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
 cd
