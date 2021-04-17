96ae968bbfacc426023f2bd7759216e28efd5cb9
manikar <42275875+manikar@users.noreply.github.com>
2021-04-17 08:51:02 -0400

    Update nobleltebmc.sh

--- nobleltebmc.sh
+++ nobleltebmc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd ~/android/lineage-17.1
+cd ~/android/lineage-18.1
 make clean
-wget -O .repo/local_manifests/roomservice.xml https://raw.githubusercontent.com/manikar/local_manifests/lineage-17.1/nobleltebmc.xml
+wget -O .repo/local_manifests/roomservice.xml https://raw.githubusercontent.com/manikar/local_manifests/lineage-18.1/nobleltebmc.xml
 repo sync -c --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
 brunch nobleltebmc
